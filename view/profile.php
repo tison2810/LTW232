d782d04e144d98de6342a4378e8354328af5db44
NhatHa <ha.ngnhnht@gmail.com>
2024-05-05 00:00:06 +0700

    xin chao mimi

--- view/profile.php
+++ view/profile.php
@@ -6,45 +6,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>demo</title>
     <!-- <script src="script.js"></script> -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../style.css">
     <link id="import_link_font_icon" rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Mulish:300,400,600,700&amp;display=swap&amp;subset=vietnamese">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
 </head>
 <?php
-  		include_once('navbar.php');
+include_once ('navbar.php');
 ?>
-    <body>
-        <div class="bgm">
+
+<body>
+    <div class="bgm">
         <br></br>
         <br></br>
         <br></br>
         <div class="profile">
-            <div class="greetname">Xin chào,
-                <h6><?php echo $authService->getLoggedInUser()->getName(); ?></h3>
+            <div class="greetname">
+                <i class="fas fa-user-circle"></i>
+                <div class="greet">
+                    Xin chào,
+                    <h6><?php echo $authService->getLoggedInUser()->getName(); ?></h6>
+                </div>
             </div>
             <div class="sidebar">
                 <div class="sidebar-item">THÔNG TIN XE</div>
-                    <a class="item-select" href="#">Xe của tôi</a>
+                <a class="item-select" href="#">Xe của tôi</a>
                 <div class="sidebar-item">ĐẶT HÀNG VÀ DỊCH VỤ</div>
-                    <a class="item-select" href="#">Lịch sử giao dịch</a><br>
-                    <a class="item-select" href="#">Bảo dưỡng sửa chữa</a><br>
-                    <a class="item-select" href="#">Thuê pin</a><br>
+                <a class="item-select" href="#">Lịch sử giao dịch</a><br>
+                <a class="item-select" href="#">Bảo dưỡng sửa chữa</a><br>
+                <a class="item-select" href="#">Thuê pin</a><br>
                 <div class="sidebar-item">TÀI KHOẢN</div>
-                    <a class="item-select" href="#">Thông tin cá nhân</a><br>
-                    <a class="item-select" href="#">Đổi mật khẩu</a><br>
-                    <a class="item-select" href="#">Đăng xuất</a><br>
+                <a class="item-select" href="#">Thông tin cá nhân</a><br>
+                <a class="item-select" href="#">Đổi mật khẩu</a><br>
+                <a class="item-select" href="#">Đăng xuất</a><br>
             </div>
             <div class="maincontent">
                 <div class="component">
                     <div class="title" style="font-weight: bold;">THÔNG TIN CÁ NHÂN</div>
-                        <div class="component-form">
-                            <div class="username" style="font-weight: bold; font-size: 14px">Họ và tên</div>
-                            <div class="email" style="font-weight: bold; font-size: 14px">Email</div>
-                            <div class="phone" style="font-weight: bold; font-size: 14px">Số điện thoại</div>
-                        </div>
-                        <br></br>
-                        <div class="password" style="font-weight: bold; font-size: 14px">Mật khẩu</div>
+                    <div class="component-form">
+                        <div class="username" style="font-weight: bold; font-size: 14px">Họ và tên</div>
+                        <div class="email" style="font-weight: bold; font-size: 14px">Email</div>
+                        <div class="phone" style="font-weight: bold; font-size: 14px">Số điện thoại</div>
+                    </div>
+                    <br></br>
+                    <div class="password" style="font-weight: bold; font-size: 14px">Mật khẩu</div>
                 </div>
                 <div class="data">
                     <!-- <br></br> -->
@@ -54,14 +61,17 @@
                         <div class="phone" style="font-size: 14px"><?= $user->getPhone(); ?></div>
                         <br></br>
                         <a class="password" style="font-size: 14px; text-decoration: none;" href="#">Đổi mật khẩu</a>
-					</div>
+                    </div>
                 </div>
                 <div class="edit">
                     <a class="edit-profile" href="#">Chỉnh sửa thông tin</a>
                 </div>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
     </div>
-    </body>
+</body>
+
 </html>
