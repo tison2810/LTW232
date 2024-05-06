b2311c4a971eb00c250c78878e8a1dd56c09b065
tison2810 <son.nguyenthai@hcmut.edu.vn>
2024-05-06 16:36:05 +0700

    xx

---
+++ view/test.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>demo</title>
+    <!-- <script src="script.js"></script> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/../style.css">
+    <link id="import_link_font_icon" rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Mulish:300,400,600,700&amp;display=swap&amp;subset=vietnamese">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+</head>
+<?php
+include_once ('navbar.php');
+?>
+<body>
+    <br></br>
+    <br></br>
+<main class="row">
+        <div class="col-lg-2">
+        </div>
+        <div class="col-lg-8">
+            <h3>SẢN PHẨM</h3>
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="card">
+                        <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/KSante_0.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">K'Sante</h5>
+                            <p class="card-price">6300 THL</p>
+                            <p class="card-text">Tướng 200 năm
+                            <p>
+                                <a href="phan1_bai2.html" class="btn btn-primary">Mua ngay</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card">
+                        <img src="img/item2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">K'Sante</h5>
+                            <p class="card-price">6300 THL</p>
+                            <p class="card-text">Tướng 200 năm
+                            <p>
+                                <a href="phan1_bai2.html" class="btn btn-primary">Mua ngay</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card">
+                        <img src="img/item2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">K'Sante</h5>
+                            <p class="card-price">6300 THL</p>
+                            <p class="card-text">Tướng 200 năm
+                            <p>
+                                <a href="phan1_bai2.html" class="btn btn-primary">Mua ngay</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card">
+                        <img src="img/item2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">K'Sante</h5>
+                            <p class="card-price">6300 THL</p>
+                            <p class="card-text">Tướng 200 năm
+                            <p>
+                                <a href="phan1_bai2.html" class="btn btn-primary">Mua ngay</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="card">
+                        <img src="img/item2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">K'Sante</h5>
+                            <p class="card-price">6300 THL</p>
+                            <p class="card-text">Tướng 200 năm
+                            <p>
+                                <a href="phan1_bai2.html" class="btn btn-primary">Mua ngay</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <nav aria-label="Page navigation example shadow-sm">
+                <ul class="pagination justify-content-end mt-3">
+                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="col-lg-2">
+        </div>
+            </div>
+        </div>
+    </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+
+</body>
+
+</html>
